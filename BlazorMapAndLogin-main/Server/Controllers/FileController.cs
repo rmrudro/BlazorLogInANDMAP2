ed930f7b4d4ce91e951b6f6639576050e72f6c9d
Rifat Murtuza <rm.rudro@gmail.com>
2023-01-10 00:46:11 +0900

    first commit

---
+++ BlazorMapAndLogin-main/Server/Controllers/FileController.cs
@@ -0,0 +1,47 @@
+﻿using BlazorWasmAuthenticationAndAuthorization.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorWasmAuthenticationAndAuthorization.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+        //private readonly DataContext _context;
+
+
+        public FileController(IWebHostEnvironment env)
+        {
+            _env = env;
+           // _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<UploadResult>>> UploadFile(List<IFormFile> files)
+        {
+            List<UploadResult> uploadResults = new List<UploadResult>();
+            foreach (var file in files)
+            {
+                var uploadResult = new UploadResult();
+                string trustedFileNameForFileStorage;
+                var untrustedFileName = file.FileName;
+                uploadResult.FileName = untrustedFileName;
+                //var trustedFileNameForDisplay = WebUtility.HtmlEncode(untrustedFileName);
+
+                trustedFileNameForFileStorage = Path.GetRandomFileName();
+                var path = Path.Combine(_env.ContentRootPath, "uploads", trustedFileNameForFileStorage);
+
+                await using FileStream fs = new(path, FileMode.Create);
+                await file.CopyToAsync(fs);
+
+                uploadResult.StoredFileName = trustedFileNameForFileStorage;
+                uploadResult.ContentType = file.ContentType;
+                uploadResults.Add(uploadResult);
+
+                //_context.Uploads.Add(uploadResult);
+            }
+            return Ok(uploadResults);
+        }
+    }
+}
---
+++ BlazorMapAndLogin-main/Shared/UploadResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWasmAuthenticationAndAuthorization.Shared
+{
+    public class UploadResult
+    {
+        public int Id { get; set; }
+        public string? FileName { get; set; }
+        public string? StoredFileName { get; set; }
+        public string? ContentType { get; set; }
+
+
+    }
+}
